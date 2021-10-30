3c3bf8dd98ad8026b77bce0c6fe91e36837945af
董浩 <barry.dong@morefun.me>
2021-10-30 20:47:16 +0800

    创建 icon 组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg class="icon">
+    <use v-bind:xlink:href="'#'+name"/>
+  </svg>
+</template>
+
+<script lang="ts">
+// 单独引入文件
+// import x from '@/assets/icons/money.svg';
+// import y from '@/assets/icons/labels.svg';
+// import z from '@/assets/icons/charts.svg';
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// 将一个目录里面任意后缀的文件全部引入到当前文件
+// eslint-disable-next-line no-undef
+let importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+  importAll(require.context('../assets/icons', true, /\.svg$/));
+} catch (error) {
+  console.log(error);
+}
+
+export default {
+  name: 'Icon',
+  props: ['name']
+}
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em; height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,34 +2,20 @@
   <div class="nav">
     <router-link to="/money">
       记账
-      <svg>
-        <use xlink:href="#money"/>
-      </svg>
+      <Icon name="money"/>
     </router-link>
     <router-link to="/labels">
       标签
-      <svg>
-        <use xlink:href="#labels"/>
-      </svg>
+      <Icon name="labels"/>
     </router-link>
     <router-link to="/statistics">
       统计
-      <svg>
-        <use xlink:href="#charts"></use>
-      </svg>
+      <Icon name="charts"/>
     </router-link>
   </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/money.svg';
-import y from '@/assets/icons/labels.svg';
-import z from '@/assets/icons/charts.svg';
-
-console.log(x);
-console.log(y);
-console.log(z);
-
 export default {
   name: 'Nav'
 };
