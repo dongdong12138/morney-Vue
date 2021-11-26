0a4bd30bcd150dd5c4c6d045a6184ba1b50158b9
董浩 <barry.dong@morefun.me>
2021-11-26 23:53:08 +0800

    完成列表的CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,10 +4,12 @@
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <ol>
       <li v-for="(group, index) in result" :key="index">
-        <h3>{{group.title}}</h3>
+        <h3 class="title">{{ group.title }}</h3>
         <ol>
-          <li v-for="item in group.items" :key="item.id">
-            {{item.amount}} -- {{item.createTime}}
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{tagString(item.tags)}}</span>
+            <span class="notes">{{item.notes}}</span>
+            <span>¥{{item.amount}}</span>
           </li>
         </ol>
       </li>
@@ -15,48 +17,29 @@
   </Layout>
 </template>
 
-<script lang="ts">
-import Tabs from '@/components/Tabs.vue';
-import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
-import intervalList from '@/constants/intervalList';
-import recordTypeList from '@/constants/recordTypeList';
-
-@Component({
-  components: {Tabs},
-})
-export default class Statistics extends Vue {
-  get recordList() {
-    // eslint-disable-next-line no-undef
-    return (this.$store.state as RootState).recordList;
-  }
-
-  get result() {
-    const {recordList} = this;
-    // eslint-disable-next-line no-undef
-    type HashTableValue = { title: string, items: RecordList[] }
-
-    let hashTable: { [key: string]: HashTableValue } = {};
-    for (let i = 0; i < recordList.length; i++) {
-      const [date] = recordList[i].createTime!.split('T');
-      hashTable[date] = hashTable[date] || {title: date, items: []};
-      hashTable[date].items.push(recordList[i]);
-    }
-    return hashTable;
-  }
-
-  created() {
-    this.$store.commit('fetchRecords');
-  }
-
-  type = '-';
-  interval = 'day';
-  intervalList = intervalList;
-  recordTypeList = recordTypeList;
-}
-</script>
-
 <style lang="scss" scoped>
+%item {
+  padding: 0 16px;
+  min-height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title {
+  @extend %item;
+}
+
+.record {
+  background: #fff;
+  @extend %item;
+}
+
+.notes {
+  margin: 0 auto 0 8px;
+  color: #999;
+}
+
 ::v-deep {
   .type-tabs-item {
     background: #fff;
@@ -75,3 +58,49 @@ export default class Statistics extends Vue {
   }
 }
 </style>
+
+<script lang="ts">
+import Tabs from '@/components/Tabs.vue';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import intervalList from '@/constants/intervalList';
+import recordTypeList from '@/constants/recordTypeList';
+
+@Component({
+  components: {Tabs},
+})
+export default class Statistics extends Vue {
+  // eslint-disable-next-line no-undef
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',');
+  }
+
+  get recordList() {
+    // eslint-disable-next-line no-undef
+    return (this.$store.state as RootState).recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    // eslint-disable-next-line no-undef
+    type HashTableValue = { title: string, items: RecordList[] }
+
+    let hashTable: { [key: string]: HashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      let [date] = recordList[i].createTime!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  created() {
+    this.$store.commit('fetchRecords');
+  }
+
+  type = '-';
+  interval = 'day';
+  intervalList = intervalList;
+  recordTypeList = recordTypeList;
+}
+</script>
